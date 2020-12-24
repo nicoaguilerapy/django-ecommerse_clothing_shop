592daabeb5fb27349047dca9b434f8e4b4b66858
uverodev <60054614+uverodev@users.noreply.github.com>
2020-12-24 12:27:23 -0300

    Actualizacion de tablas

--- contact/views.py
+++ contact/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail
 from .forms import ContactForm
 from social.models import Fact, Email
 
 def contact(request):
 	email_obj = Email.objects.get(id=1)
+	dato_email_asunto = Fact.objects.get(slug = "dato_email_asunto")
 	correo_contacto = email_obj.email
 	contact_form = ContactForm()
 	if request.method == 'POST':
 		nombre = request.POST.get('nombre')
 		correo = request.POST.get('correo')
 		contenido = request.POST.get('contenido')
-		email = EmailMessage(
-			"TU TIENDA, nuevo msj desde Contacto",
-			"De {} <{}>\n\nMensaje: \n\n{}".format(nombre, correo, contenido),
-			"no-responder@uverodev.com",
-			[correo_contacto],
-			reply_to=[correo]
-		)
 		try:
-			email.send()
+			send_mail(
+    			dato_email_asunto.value,
+				"De {} <{}>\n\nMensaje: \n\n{}".format(nombre, correo, contenido),
+    			correo_contacto,
+    			[correo_contacto, correo],
+    					fail_silently=False,
+				)
 			return redirect(reverse('contacto')+"?send=ok")
 		except:
 			print
--- minishop/produccion.py
+++ minishop/produccion.py
@@ -8,10 +8,6 @@ ALLOWED_HOSTS = ["*"]
 # Email
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'aventurerosdeleste@gmail.com'
-EMAIL_HOST_PASSWORD = 'brdwubrlavflgmfx'
+EMAIL_HOST_PASSWORD = 'blcqtxbxlawbvwsj'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 2
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
