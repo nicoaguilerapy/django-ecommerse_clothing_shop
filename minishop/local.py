3f3c073301de71507af54a7262fa613c166b555c
uverodev <60054614+uverodev@users.noreply.github.com>
2021-07-26 15:39:23 -0400

    Update local.py

--- minishop/local.py
+++ minishop/local.py
@@ -13,8 +13,10 @@ DATABASES = {
 }
 
 # Email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'aventurerosdeleste@gmail.com'
-EMAIL_HOST_PASSWORD = 'blcqtxbxlawbvwsj'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'aventurerosdeleste@gmail.com'
+# EMAIL_HOST_PASSWORD = 'blcqtxbxlawbvwsj'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
