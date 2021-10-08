8e4ad72ba198749d4690a9b6a1be908b115e7fa6
uverodev <60054614+uverodev@users.noreply.github.com>
2021-10-08 11:28:47 -0300

    debug true postgres

--- minishop/settings.py
+++ minishop/settings.py
@@ -19,7 +19,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = False
+DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
