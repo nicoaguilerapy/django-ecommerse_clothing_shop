5eaba4d25c0c2aad7121f64134780b88d59a5128
uverodev <60054614+uverodev@users.noreply.github.com>
2021-10-08 11:26:00 -0300

    debug false

--- minishop/settings.py
+++ minishop/settings.py
@@ -19,7 +19,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
